36a231744bf2d1ce0b36ff1665c6c15c4f496750
李照宇 <38952251+ApolloLiZhaoyu@users.noreply.github.com>
2019-03-14 16:20:53 +0800

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,6 +93,8 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    int64_t ticks_blocked;
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -137,5 +139,7 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+
+void blocked_thread_check (struct thread *t, void *aux UNUSED);
 
 #endif /* threads/thread.h */
